bc4cc50da1181e86a89eafc4b3f0389acee7fccc
mattromano <mattromano88@gmail.com>
2024-06-27 12:00:01 -0700

    ez native

---
+++ models/gold/core/core__ez_native_transfers.sql
@@ -0,0 +1,32 @@
+{{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true }
+) }}
+
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    tx_position,
+    trace_index,
+    identifier,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    from_address,
+    to_address,
+    amount,
+    amount_precise_raw,
+    amount_precise,
+    amount_usd,
+    COALESCE (
+        native_transfers_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['tx_hash', 'trace_index']
+        ) }}
+    ) AS ez_native_transfers_id,
+    inserted_timestamp,
+    modified_timestamp
+FROM
+    {{ ref('silver__native_transfers') }}
