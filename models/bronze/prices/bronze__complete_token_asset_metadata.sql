063b14fa5b869b27cc18b2180a71ef2b40eb6c4b
mattromano <mattromano88@gmail.com>
2024-07-09 14:26:43 -0700

    update metadata

--- models/bronze/prices/bronze__complete_token_asset_metadata.sql
+++ models/bronze/prices/bronze__complete_token_asset_metadata.sql
@@ -25,4 +25,31 @@ FROM
         'complete_token_asset_metadata'
     ) }}
 WHERE
-    blockchain IN ('klaytn','kaia','klay token')
+    blockchain IN ('klaytn','kaia')
+UNION ALL
+SELECT
+    token_address,
+    asset_id,
+    symbol,
+    NAME,
+    decimals,
+    blockchain,
+    blockchain_name,
+    blockchain_id,
+    is_deprecated,
+    provider,
+    source,
+    _inserted_timestamp,
+    inserted_timestamp,
+    modified_timestamp,
+    complete_token_asset_metadata_id,
+    _invocation_id
+FROM
+    {{ source(
+        'silver_crosschain',
+        'complete_token_asset_metadata'
+    ) }}
+WHERE
+    blockchain = 'klay token'
+AND 
+    token_address = '0xe4f05a66ec68b54a58b17c22107b02e0232cc817'
