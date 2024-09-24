4fec3cc0abda52fd85cb243c4a48825cb97a46ec
Austin <austin@flipsidecrypto.com>
2024-09-24 09:06:44 -0400

    params

--- models/silver/core/silver__traces2.sql
+++ models/silver/core/silver__traces2.sql
@@ -9,7 +9,7 @@
 ) }}
 
 {{ fsc_evm.silver_traces_v1(
-    full_reload_start_block = 9000000,
+    full_reload_start_block = 20000000,
     full_reload_blocks = 25000000,
     full_reload_mode = true,
     use_partition_key = true
