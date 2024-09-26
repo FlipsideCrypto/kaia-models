b9a36c1f4313b8af55adb1c56a26e0f18c3f1056
Austin <austin@flipsidecrypto.com>
2024-09-26 11:37:33 -0400

    kaia mode

--- models/silver/core/silver__fact_traces2.sql
+++ models/silver/core/silver__fact_traces2.sql
@@ -1,16 +1,16 @@
--- depends_on: {{ ref('bronze__streamline_traces') }}
 {{ config (
     materialized = "incremental",
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
-    cluster_by = ['modified_timestamp::DATE','partition_key'],
+    incremental_predicates = [fsc_evm.standard_predicate()],
+    cluster_by = "block_timestamp::date",
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
     tags = ['traces_reload']
 ) }}
-
-{{ fsc_evm.silver_traces_v1(
+{{ fsc_evm.gold_traces_v1(
     full_reload_start_block = 149500000,
     full_reload_blocks = 5000000,
     full_reload_mode = true,
-    use_partition_key = true,
+    tx_status_bool = true,
     kaia_traces_mode = true
 ) }}
