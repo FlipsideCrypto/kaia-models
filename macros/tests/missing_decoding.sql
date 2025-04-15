1cf00d6c0100a6d333a46c9b7011c98efc518891
mattromano <mattromano88@gmail.com>
2025-04-15 10:47:20 -0700

    fix missing decoded

--- macros/tests/missing_decoding.sql
+++ macros/tests/missing_decoding.sql
@@ -14,11 +14,11 @@ FROM
         l.event_index
     ) = d._log_id
 WHERE
-    l.contract_address = LOWER('0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270') -- WMATIC
+    LOWER('0x19aac5f612f524b754ca7e7c41cbfa2e981a4432') -- WKLAY
     AND l.topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- Transfer
     AND l.block_timestamp BETWEEN DATEADD('hour', -48, SYSDATE())
     AND DATEADD('hour', -6, SYSDATE())
-    AND d._log_id IS NULL {% endtest %}
+    AND d._log_id IS NULL
 {% endtest %}
 
 {% test missing_gold_decoded_logs(model) %}
